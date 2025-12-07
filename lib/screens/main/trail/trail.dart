49dea7d2106b680fd086ee8e65669692ce3bae05
Tharindu Akalanka <tharinduhccakalanka@gmail.com>
2025-12-07 23:05:43 +0530

    added a new file

---
+++ lib/screens/main/trail/trail.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Trail extends StatefulWidget {
+  const Trail({super.key});
+
+  @override
+  State<Trail> createState() => _TrailState();
+}
+
+class _TrailState extends State<Trail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Trail Screen",
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
